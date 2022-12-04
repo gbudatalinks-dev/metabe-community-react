0cd3289f11685231054105ba44a0c4ae2330e5d8
Myeongkoo Kang <myeongkoo.kang@goozsoft.com>
2022-12-04 13:33:20 +0900

    [2022/12/04] UI 변경작업 업데이트 #7

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Layer, Box, Text, Button, TextArea } from "grommet";
+import {Layer, Box, Text, Button, FileInput, TextArea, Image, TextInput} from "grommet";
 import {
     TbCirclePlus, TbPhoto, TbHandClick, TbHeading, TbLetterT, TbLayoutDistributeHorizontal
 } from "react-icons/tb";
@@ -11,8 +11,8 @@ import {
 //     </Box>
 // );
 
-function auto_grow(compRef) {
-    compRef.current.style.height = "42px";
+function auto_grow(compRef, initHeight) {
+    compRef.current.style.height = `${initHeight}px`;
     compRef.current.style.height = (compRef.current.scrollHeight)+"px";
 }
 
@@ -28,22 +28,69 @@ export default function Editor() {
 
     const [ children, setChildren ] = React.useState([]);
 
-    const onChangeAlign = (compRef) => {
-        // const element = document.getElementById(id);
-        console.log(compRef.current);
-        compRef.current.style.alignSelf = "center";
+    const [ imageUploadOpen, setImageUploadOpen ] = React.useState(false);
+    const onImageUploadClose = () => setImageUploadOpen(false);
+
+    const [ buttonLabel, setButtonLabel ] = React.useState(undefined);
+    const [ buttonLink, setButtonLink ] = React.useState(undefined);
+    const [ buttonMenuOpen, setButtonMenuOpen ] = React.useState(false);
+    const onButtonMenuClose = () => setButtonMenuOpen(false);
+
+    // const onChangeAlign = (compRef) => {
+    //     // const element = document.getElementById(id);
+    //     console.log(compRef.current);
+    //     compRef.current.style.alignSelf = "center";
+    // };
+
+    const openImageUpload = () => {
+        onContextMenuClose();
+        setImageUploadOpen(true);
     };
 
-    const addImage = () => {
+    const addImage = (file) => {
+        // TODO : Upload image file to firebase
+        let reader = new FileReader();
+        reader.onloadend = () => {
+            const imageSource = reader.result;
+
+            let newChildren = children;
+            const newRef = React.createRef();
+            newChildren.push({
+                key: nextId,
+                ref: newRef,
+                component: (
+                    <Box key={nextId.current} ref={newRef}
+                         margin={{ vertical: "xsmall" }}
+                         round={"medium"} overflow={"hidden"}
+                         border={{ size: "xsmall", color: "brand", style: "dashed" }}
+                    >
+                        <Image style={{ borderRadius: 24 }}
+                               src={imageSource}
+                               fill={"horizontal"}
+                            // fit={"contain"}
+                        />
+                    </Box>
+                )
+            });
+            setChildren(newChildren);
+            onImageUploadClose();
+            nextId.current += 1;
+        };
+        reader.readAsDataURL(file);
+    };
+
+    const addHeading = () => {
         let newChildren = children;
         const newRef = React.createRef();
         newChildren.push({
             key: nextId,
             ref: newRef,
             component: (
-                <Box key={nextId.current} ref={newRef} className={"disabled-focus"} onClick={() => onChangeAlign(newRef)}>
-                    <Text size={"small"}>Inserted</Text>
-                </Box>
+                <TextArea key={nextId.current} ref={newRef}
+                          size={"large"} style={{ color: "#ff5a01", fontSize: 18, width: "100%", height: 50, marginTop: 6, border: "1px dashed #ff5a01" }}
+                          onInput={() => auto_grow(newRef, 50)}
+                          onBlur={() => console.log("done")}
+                />
             )
         });
         setChildren(newChildren);
@@ -59,8 +106,8 @@ export default function Editor() {
             ref: newRef,
             component: (
                 <TextArea key={nextId.current} ref={newRef}
-                          size={"small"} style={{ width: "100%", height: 42, border: "1px dashed #ff5a01" }}
-                          onInput={() => auto_grow(newRef)}
+                          size={"small"} style={{ width: "100%", fontSize: 12, height: 42, marginTop: 6, border: "1px dashed #ff5a01" }}
+                          onInput={() => auto_grow(newRef, 42)}
                           onBlur={() => console.log("done")}
                 />
             )
@@ -70,8 +117,57 @@ export default function Editor() {
         nextId.current += 1;
     };
 
+    const openButtonMenu = () => {
+        onContextMenuClose();
+        setButtonMenuOpen(true);
+    };
+
+    const addButton = () => {
+        let newChildren = children;
+        const newRef = React.createRef();
+        newChildren.push({
+            key: nextId,
+            ref: newRef,
+            component: (
+                <Box key={nextId.current} ref={newRef}
+                     margin={{ vertical: "xsmall" }}
+                     round={"medium"} align={"center"}
+                >
+                    <Button primary label={<Text color={"#eeeeee"} size={"small"}>{buttonLabel}</Text>} />
+                </Box>
+            )
+        });
+        setChildren(newChildren);
+        onButtonMenuClose();
+        setButtonLabel(undefined);
+        setButtonLink(undefined);
+        nextId.current += 1;
+    };
+
+    const addHorizontalLine = () => {
+        let newChildren = children;
+        const newRef = React.createRef();
+        newChildren.push({
+            key: nextId,
+            ref: newRef,
+            component: (
+                <Box key={nextId.current} ref={newRef}
+                     height={"1px"}
+                     margin={{ vertical: "small" }}
+                     round={"medium"} overflow={"hidden"}
+                     background={"brand"}
+                />
+            )
+        });
+        setChildren(newChildren);
+        onContextMenuClose();
+        nextId.current += 1;
+    };
+
     return (
-        <Box round={"medium"} pad={"medium"} flex background={"background-front"} overflow={"scroll"} ref={ref}>
+        <Box round={"medium"} pad={{ vertical: "small", horizontal: "medium" }} align={"start"} background={"background-front"} overflow={"scroll"} ref={ref}
+             style={{ display: "block", height: "100%" }}
+        >
             { contextMenuOpen &&
                 <Layer position={"center"} background={"background"}
                        target={ref.current}
@@ -85,30 +181,69 @@ export default function Editor() {
                         {/*    <Button icon={<TbLayoutColumns />} hoverIndicator onClick={() => {}} />*/}
                         {/*</Box>*/}
                         <Box round={"medium"} overflow={"hidden"}>
-                            <Button icon={<TbPhoto />} hoverIndicator onClick={() => addImage()} />
+                            <Button icon={<TbPhoto />} hoverIndicator onClick={() => openImageUpload()} />
                         </Box>
                         <Box round={"medium"} overflow={"hidden"}>
-                            <Button icon={<TbHandClick />} hoverIndicator onClick={() => {}} />
+                            <Button icon={<TbHandClick />} hoverIndicator onClick={() => openButtonMenu()} />
                         </Box>
                         <Box round={"medium"} overflow={"hidden"}>
-                            <Button icon={<TbHeading />} hoverIndicator onClick={() => {}} />
+                            <Button icon={<TbHeading />} hoverIndicator onClick={() => addHeading()} />
                         </Box>
                         <Box round={"medium"} overflow={"hidden"}>
                             <Button icon={<TbLetterT />} hoverIndicator onClick={() => addParagraph()} />
                         </Box>
                         <Box round={"medium"} overflow={"hidden"}>
-                            <Button icon={<TbLayoutDistributeHorizontal />} hoverIndicator onClick={() => {}} />
+                            <Button icon={<TbLayoutDistributeHorizontal />} hoverIndicator onClick={() => addHorizontalLine()} />
+                        </Box>
+                    </Box>
+                </Layer>
+            }
+            { imageUploadOpen &&
+                <Layer position={"center"} background={"background"}
+                       target={ref.current}
+                       style={{ width: "100%", height: 120, alignSelf: "center", borderRadius: 24 }}
+                       onClickOutside={onImageUploadClose} onEsc={onImageUploadClose}
+                       modal
+                       responsive
+                >
+                    <FileInput
+                        accept="image/*"
+                        messages={{
+                            browse: "선택",
+                            dropPrompt: "이미지 파일을 여기에 드래그 하세요.",
+                            remove: "삭제",
+                        }}
+                        onChange={(event, { files }) => {
+                            addImage(files[0]);
+                        }}
+                    />
+                </Layer>
+            }
+            { buttonMenuOpen &&
+                <Layer position={"center"} background={"background"}
+                       target={ref.current}
+                       style={{ alignSelf: "center", borderRadius: 24 }}
+                       onClickOutside={onButtonMenuClose} onEsc={onButtonMenuClose}
+                       modal
+                       responsive
+                >
+                    <Box width={"360px"} gap={"small"} pad={"medium"} round={"medium"} border={{ color: "background-front", size: "xsmall" }}>
+                        <TextInput size={"small"} placeholder={"버튼 레이블"} onChange={e => setButtonLabel(e.target.value)} />
+                        <TextInput size={"small"} placeholder={"버튼 링크 URL"} onChange={e => setButtonLink(e.target.value)} />
+                        <Box align={"center"}>
+                            <Button primary label={<Text color={"#eeeeee"} size={"small"}>등록</Text>} onClick={() => addButton()} />
                         </Box>
                     </Box>
                 </Layer>
             }
             { children.map((child, index) => child.component) }
-            <Box id={"placeholder"} width={"100%"} flex align={"center"} justify={"center"} className={"disabled-focus"}
+            <Box width={"100%"} pad={"medium"} margin={{ vertical: "small" }} align={"center"} justify={"center"} round={"medium"}
+                 className={"disabled-focus"} border={{ size: "xsmall", color: "text-weak", style: "dashed" }}
                  onClick={onContextMenuOpen}
             >
                 <Box direction={"row"} gap={"xsmall"}>
                     <TbCirclePlus color={"#999999"} />
-                    <Text size={"small"} color={"text-xweak"}> 클릭하여 내용을 추가해 주세요.</Text>
+                    <Text size={"12px"} color={"text-xweak"}> 클릭하여 내용을 추가해 주세요.</Text>
                 </Box>
             </Box>
         </Box>
